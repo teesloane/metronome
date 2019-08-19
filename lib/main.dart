0d2881324acc26cd18d3e5c4621b500abd4d09be
teesloane <tylersloane@gmail.com>
2019-08-18 20:07:13 -0400

    Move slider to right, some styling.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:metronome/tempoSlider.dart';
+import 'package:metronome/util.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,17 +15,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.lightGreen,
-      ),
+          primarySwatch: Colors.lightGreen,
+          textTheme: Theme.of(context).textTheme.apply(
+                // fontFamily: 'Open Sans', // TODO: set a font.
+                bodyColor: Colors.white,
+                displayColor: Colors.white,
+              )),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -32,16 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,10 +42,12 @@ class _MyHomePageState extends State<MyHomePage> {
   Timer _timer;
   int _beat = 1;
   int _bar = 4;
-  Duration _tempo = Duration(milliseconds: 500);
+  int _tempoInt = 120;
+  double maxTempo = 300;
+  double minTempo = 30;
+  Duration _tempoDuration = Duration(milliseconds: 500);
   bool _isRunning = false;
-  double _tempo2 = 0;
-  double _sliderOffset = 20;
+  double _sliderOffset = 50;
 
   // Methods --
 
@@ -76,25 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  // _onVerticalGesture(DragUpdateDetails details) {
-  //   setState(() {
-  //     _tempo += Duration(milliseconds: details.delta.dy.round());
-  //   });
-  // }
-
-  // _onReleaseTempoSlider() {
-  //   // If running, stop it, adjust tempo with new timer and resume
-  //   if (_isRunning) {
-  //     _timer.cancel();
-  //     setState(() {
-  //       _timer = Timer.periodic(_tempo, _metroInc);
-  //     });
-  //   } else {
-  //     print("do nothing"); // Remove at some point.
-  //   }
-  // }
-
-  _toggleTimer() {
+  void _toggleTimer() {
     if (_isRunning) {
       setState(() {
         _timer.cancel();
@@ -102,13 +72,34 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     } else {
       setState(() {
-        _timer = Timer.periodic(_tempo, _metroInc);
+        _timer = Timer.periodic(_tempoDuration, _metroInc);
         _isRunning = true;
       });
     }
   }
 
-  _buildToggleButton() {
+  _setTempo(double sliderVal) {
+    var _newTempo = (sliderVal * 100).toInt();
+    var _scaledTempo =
+        scaleNum(_newTempo, 0, 100, bpmToMS(minTempo), bpmToMS(maxTempo));
+
+    // if running, cancel timer and restart it.
+    if (_isRunning) {
+      _timer.cancel();
+      setState(() {
+        _tempoDuration = Duration(milliseconds: _scaledTempo.toInt());
+        _tempoInt = msToBpm(_scaledTempo).toInt();
+        _timer = Timer.periodic(_tempoDuration, _metroInc);
+      });
+    } else {
+      setState(() {
+        _tempoDuration = Duration(milliseconds: _scaledTempo.toInt());
+        _tempoInt = msToBpm(_scaledTempo).toInt();
+      });
+    }
+  }
+
+  MaterialButton _buildToggleButton() {
     if (!_isRunning) {
       return MaterialButton(
           child: Text("Start"),
@@ -127,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   String getTempo() {
-    var sliceOfString = _tempo.toString().substring(8, 11);
+    var sliceOfString = _tempoDuration.toString().substring(8, 11);
     var intTempo = (60000 / int.parse(sliceOfString)).round();
     return intTempo.toString();
   }
@@ -135,83 +126,41 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   title: Text(widget.title),
-        // ),
+        backgroundColor: Colors.white.withOpacity(0.1),
         body: Center(
-      child: Stack(children: <Widget>[
-        Container(), // Makes the stack full screen size.
-        Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  '$_beat / $_bar',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                _buildToggleButton(),
-                Text(
-                  getTempo(),
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                Text(_tempo2.toString()),
-
-                // InteractableWidget,
-              ]),
-        ),
-        // TempoScroller(
-        //   notifyParent: _onVerticalGesture,
-        //   handleOVDE: _onReleaseTempoSlider,
-        // )
-
-        Positioned(
-          right: 0,
-          bottom: _sliderOffset,
-          // top: 50,
-          child: RotatedBox(
-            quarterTurns: 3,
-            child: TempoSlider(
-              width: MediaQuery.of(context).size.height - (_sliderOffset * 2),
-              color: Colors.red,
-              onChanged: (double val) {
-                setState(() {
-                  _tempo2 = (val * 100).roundToDouble();
-                });
-              },
-              onChangedStart: (double val) {
-                setState(() {
-                  _tempo2 = (val * 100).roundToDouble();
-                });
-              },
+          child: Stack(children: <Widget>[
+            Container(), // Makes the stack full screen size.
+            Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('$_beat / $_bar',
+                      style: Theme.of(context).textTheme.display4,
+                    ),
+                    _buildToggleButton(),
+                    Text(
+                      _tempoInt.toString(),
+                      style: Theme.of(context).textTheme.display4,
+                    ),
+                    // InteractableWidget,
+                  ]),
             ),
-          ),
-        )
-      ]),
-    ));
-  }
-}
 
-class TempoScroller extends StatelessWidget {
-  final Function(DragUpdateDetails details) notifyParent;
-  final Function() handleOVDE;
-  TempoScroller(
-      {Key key, @required this.notifyParent, @required this.handleOVDE})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-        top: 0,
-        bottom: 0,
-        right: 0,
-        child: Container(
-            width: 45.0,
-            child: GestureDetector(
-                child: Container(color: Colors.lightGreen.withOpacity(0.3)),
-                onVerticalDragEnd: (e) => handleOVDE(),
-                onVerticalDragDown: (e) => print(e),
-                onVerticalDragUpdate: (e) {
-                  notifyParent(e);
-                })));
+            Positioned(
+              right: 0,
+              bottom: _sliderOffset,
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: TempoSlider(
+                  width:
+                      MediaQuery.of(context).size.height - (_sliderOffset * 2),
+                  color: Colors.orange,
+                  onChanged: (val) => _setTempo(val),
+                  onChangedStart: (val) => _setTempo(val),
+                ),
+              ),
+            )
+          ]),
+        ));
   }
 }
---
+++ lib/util.dart
@@ -0,0 +1,17 @@
+/// scales a number in a range to another range.
+/// - How can this be made to work with any number of number types
+/// TODO - make this use generics.
+double scaleNum(num, inMin, inMax, outMin, outMax) {
+  return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+
+// These are the same but, for clarity...
+
+msToBpm(double ms) {
+  return 60000 / ms;
+}
+
+bpmToMS(double bpm) {
+  return  60000 / bpm;
+}
